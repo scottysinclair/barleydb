aaf77fc2345fcae023f5ea301ccf09468722e47b
Scott Sinclair <scottysinclair@gmail.com>
2016-09-09 20:58:35 +0200

    etl play

--- src/test/java/scott/barleydb/test/TestEtl.java
+++ src/test/java/scott/barleydb/test/TestEtl.java
@@ -1,5 +1,10 @@
 package scott.barleydb.test;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 /*-
  * #%L
  * BarleyDB
@@ -13,12 +18,12 @@ package scott.barleydb.test;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -32,6 +37,8 @@ import org.example.etl.query.QXmlMapping;
 import org.example.etl.query.QXmlSyntaxModel;
 import org.junit.Test;
 
+import scott.barleydb.api.config.EntityType;
+import scott.barleydb.api.config.NodeType;
 import scott.barleydb.api.core.entity.Entity;
 import scott.barleydb.api.core.entity.EntityContext;
 import scott.barleydb.api.core.entity.ValueNode;
@@ -41,20 +48,31 @@ import scott.barleydb.api.persist.PersistRequest;
 import scott.barleydb.api.query.QueryObject;
 import scott.barleydb.server.jdbc.query.QueryResult;
 
+
+/**
+ * requirements:
+ *   - track rows whch failed when transforming
+ *   - report on mappings
+ *
+ *
+ * @author scott
+ *
+ */
+
 public class TestEtl extends TestBase {
 
     @Test
     public void testEtlXmlSyntax() throws SortException {
         EntityContext ctx = new MiEntityContext(env);
-        EtlTransform<XmlSyntaxModel, XmlSyntaxModel> transform = new EtlTransform<>(ctx, XmlSyntaxModel.class, XmlSyntaxModel.class);
-        transform.execute(new QXmlSyntaxModel());
+        EtlTransform<XmlSyntaxModel, XmlSyntaxModel> transform = new EtlTransform<>(null, ctx, XmlSyntaxModel.class, XmlSyntaxModel.class);
+        transform.executeLeftToRight(new QXmlSyntaxModel());
     }
 
     @Test
     public void testEtlXmlMapping() throws SortException {
         EntityContext ctx = new MiEntityContext(env);
-        EtlTransform<XmlMapping, XmlMapping> transform = new EtlTransform<>(ctx, XmlMapping.class, XmlMapping.class);
-        transform.execute(new QXmlMapping());
+        EtlTransform<XmlMapping, XmlMapping> transform = new EtlTransform<>(null, ctx, XmlMapping.class, XmlMapping.class);
+        transform.executeLeftToRight(new QXmlMapping());
     }
 
 
@@ -66,26 +84,81 @@ public class TestEtl extends TestBase {
 
 }
 
-class EtlTransform<SRC,DEST> {
+class EtlConfiguration {
+    private final Set<EntityMapping> entityMappings = new HashSet<>();
 
+}
+
+class EntityMapping {
+    private final EntityType left;
+    private final EntityType right;
+    private final List<NodeMapping> mappings = new LinkedList<>();
+
+    public EntityMapping(EntityType left, EntityType right) {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class NodeMapping {
+    private final NodeType left;
+    private final NodeType right;
+    private final DataTransformation transform;
+
+    public NodeMapping(NodeType left, NodeType right, DataTransformation transform) {
+        this.left = left;
+        this.right = right;
+        this.transform = transform;
+    }
+
+}
+
+interface DataTransformation {
+  Object transformFromLeftToRight(Object data) throws DataTransformationException;
+  Object transformFromRightToLeft(Object data) throws DataTransformationException;
+}
+
+
+class NoopDataTransformation implements DataTransformation {
+
+    @Override
+    public Object transformFromLeftToRight(Object data) throws DataTransformationException {
+        return data;
+    }
+
+    @Override
+    public Object transformFromRightToLeft(Object data) throws DataTransformationException {
+        return data;
+    }
+
+}
+
+class SortEtlException extends SortException {}
+class DataTransformationException extends SortEtlException {}
+
+class EtlTransform<LEFT,RIGHT> {
+
+    private final EtlConfiguration configuration;
     private final EntityContext ctx;
 
-    public EtlTransform(EntityContext ctx, Class<SRC> class1, Class<DEST> class2) {
+
+    public EtlTransform(EtlConfiguration configuration, EntityContext ctx, Class<LEFT> class1, Class<RIGHT> class2) {
+        this.configuration = configuration;
         this.ctx = ctx;
     }
 
-    public void execute(QueryObject<SRC> query) throws SortException  {
+    public void executeLeftToRight(QueryObject<LEFT> query) throws SortException  {
         PersistRequest pr = new PersistRequest();
-        QueryResult<SRC>result  = ctx.performQuery(query);
+        QueryResult<LEFT>result  = ctx.performQuery(query);
         for (Entity entity: result.getEntityList()) {
-            pr.save( transform(entity) );
+            pr.save( transformLeftToRight(entity) );
         }
         if (!pr.isEmpty()) {
             ctx.persist(pr);
         }
     }
 
-    private Entity transform(Entity entity) {
+    private Entity transformLeftToRight(Entity entity) {
         for (ValueNode vn: entity.getChildren(ValueNode.class)) {
             if (vn.getNodeType().getJavaType() == JavaType.STRING) {
                 vn.setValue( vn.getValue() + "!");
---
+++ src/test/java/scott/barleydb/test/TestEtlDozer.java
@@ -0,0 +1,218 @@
+package scott.barleydb.test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.dozer.BeanFactory;
+import org.dozer.DozerBeanMapper;
+import org.dozer.classmap.MappingFileData;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.dozer.loader.api.TypeMappingOption;
+import org.dozer.loader.api.TypeMappingOptions;
+import org.dozer.metadata.ClassMappingMetadata;
+import org.dozer.metadata.FieldMappingMetadata;
+import org.example.acl.model.AccessArea;
+import org.example.acl.model.User;
+
+/*-
+ * #%L
+ * BarleyDB
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 - 2016 Scott Sinclair
+ *       <scottysinclair@gmail.com>
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import org.example.etl.context.MiEntityContext;
+import org.example.etl.model.XmlMapping;
+import org.example.etl.model.XmlStructure;
+import org.example.etl.model.XmlSyntaxModel;
+import org.example.etl.query.QXmlMapping;
+import org.example.etl.query.QXmlSyntaxModel;
+import org.junit.Before;
+import org.junit.Test;
+
+import scott.barleydb.api.config.EntityType;
+import scott.barleydb.api.config.NodeType;
+import scott.barleydb.api.core.entity.Entity;
+import scott.barleydb.api.core.entity.EntityConstraint;
+import scott.barleydb.api.core.entity.EntityContext;
+import scott.barleydb.api.core.entity.EntityContextState;
+import scott.barleydb.api.core.entity.ValueNode;
+import scott.barleydb.api.core.types.JavaType;
+import scott.barleydb.api.exception.SortException;
+import scott.barleydb.api.persist.PersistRequest;
+import scott.barleydb.api.query.QueryObject;
+import scott.barleydb.server.jdbc.query.QueryResult;
+
+
+/**
+ * requirements:
+ *   - track rows whch failed when transforming
+ *   - report on mappings
+ *
+ *
+ * @author scott
+ *
+ */
+public class TestEtlDozer extends TestBase {
+
+    private EntityContext ctxSource;
+    private EntityContext ctxDest;
+    private EtlDozerConfiguration cfg;
+
+    @Before
+    public void setup() throws Exception {
+        super.setup();
+        ctxSource = new MiEntityContext(env);
+        ctxDest = new MiEntityContext(env);
+        cfg = new EtlDozerConfiguration(ctxDest);
+    }
+
+    @Test
+    public void testEtlXmlSyntax() throws SortException {
+        System.out.println(cfg);
+        EtlDozerExecution exec = new EtlDozerExecution(cfg, ctxSource, ctxDest, XmlSyntaxModel.class, XmlSyntaxModel.class);
+        exec.executeLeftToRight(new QXmlSyntaxModel());
+    }
+
+    @Test
+    public void testEtlXmlMapping() throws SortException {
+    }
+
+
+    @Override
+    protected void prepareData() throws Exception {
+        super.prepareData();
+        executeScript("/inserts.sql", false);
+    }
+
+}
+
+class EtlDozerConfiguration {
+
+    private DozerBeanMapper mapper;
+    private BeanMappingBuilder builder;
+
+    public EtlDozerConfiguration(final EntityContext ctx) {
+        builder = new BeanMappingBuilder() {
+              protected void configure() {
+                mapping(XmlSyntaxModel.class, XmlSyntaxModel.class, TypeMappingOptions.beanFactory("MYBF"));
+                mapping(XmlStructure.class, XmlStructure.class, TypeMappingOptions.beanFactory("MYBF"));
+                mapping(AccessArea.class, AccessArea.class, TypeMappingOptions.beanFactory("MYBF"));
+                mapping(User.class, User.class, TypeMappingOptions.beanFactory("MYBF"));
+                mapping(XmlMapping.class, XmlMapping.class, TypeMappingOptions.beanFactory("MYBF"));
+              }
+        };
+        builder.build();
+
+        mapper = new DozerBeanMapper();
+        mapper.addMapping(builder);
+        Map<String, BeanFactory> facs = new HashMap<String,BeanFactory>();
+
+        final BeanFactory bf = new BeanFactory() {
+            @Override
+            public Object createBean(Object source, Class<?> sourceClass, String targetBeanId) {
+                try {
+                    return ctx.newModel(Class.forName(targetBeanId), EntityConstraint.dontFetch());
+                } catch (ClassNotFoundException x) {
+                    throw new IllegalStateException("Could not load class " + targetBeanId);
+                }
+            }
+        };
+
+        facs.put("MYBF", bf);
+        mapper.setFactories(facs);
+    }
+
+    public <T> T map(Object object, Class<T> destinationClass) {
+        return mapper.map(object, destinationClass);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (ClassMappingMetadata cmmd: mapper.getMappingMetadata().getClassMappings()) {
+            sb.append("class mapping from " + cmmd.getSourceClassName() + " to " + cmmd.getDestinationClassName() + "\n");
+            for (FieldMappingMetadata fmmd: cmmd.getFieldMappings()) {
+                sb.append("  from " + fmmd.getSourceName() + " to " + fmmd.getDestinationName() + " cnv:" + fmmd.getCustomConverter() + "\n");
+            }
+        }
+        return sb.toString();
+    }
+
+
+}
+
+
+class EtlDozerExecution<LEFT,RIGHT> {
+
+    private final EtlDozerConfiguration configuration;
+    private final EntityContext ctxSource;
+    private final EntityContext ctxDest;
+    private final Class<RIGHT> rightClass;
+
+
+    public EtlDozerExecution(EtlDozerConfiguration configuration, EntityContext ctxSource, EntityContext ctxDest, Class<LEFT> leftClass, Class<RIGHT> rightClass) {
+        this.configuration = configuration;
+        this.ctxSource = ctxSource;
+        this.ctxDest = ctxDest;
+        this.rightClass = rightClass;
+    }
+
+    public void executeLeftToRight(QueryObject<LEFT> query) throws SortException  {
+        PersistRequest pr = new PersistRequest();
+        QueryResult<LEFT>result  = ctxSource.performQuery(query);
+//        EntityContextState state = ctxDest.beginLoading();
+        try {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("=============== QUERY FINISHED");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("=============== TRANSFORM STARTED");
+            for (LEFT value: result.getList()) {
+                pr.save( transformLeftToRight(value) );
+            }
+            System.out.println("");
+            System.out.println("");
+            System.out.println("=============== TRANSFORM FINISHED");
+            if (!pr.isEmpty()) {
+                System.out.println("");
+                System.out.println("");
+                System.out.println("=============== PERIST STARTED ");
+                ctxDest.persist(pr);
+                System.out.println("");
+                System.out.println("");
+                System.out.println("=============== PERIST FINISHED");
+            }
+        }
+        finally {
+  //          ctxDest.setEntityContextState(state);
+        }
+    }
+
+    private RIGHT transformLeftToRight(LEFT left) {
+        return configuration.map(left, rightClass);
+    }
+
+}
